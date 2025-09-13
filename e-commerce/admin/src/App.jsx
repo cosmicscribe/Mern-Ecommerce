52a8d918797df958216a770ab830a858df163311
Anirban Mondal <anirbanmondal.reach@gmail.com>
2025-09-13 19:42:37 +0530

    Update App.jsx

--- e-commerce/admin/src/App.jsx
+++ e-commerce/admin/src/App.jsx
@@ -11,9 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-// export const currency = (price) => {
-//   return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price);
-// };
+
 export const currency = (price) => {
   return new Intl.NumberFormat("en-IN", {
     style: "currency",
